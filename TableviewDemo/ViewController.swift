e12373e039179ec15031f247f1e6429bff1bb080
Pooja <pooja.q.gupta@accenture.com>
2020-01-13 11:09:46 +0530

    first commit

---
+++ TableviewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableviewDemo
+//
+//  Created by Pooja Gupta on 13/01/20.
+//  Copyright © 2020 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
